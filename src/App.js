01c0f3213e5bf4431480f6338ae230dbe36aec56
Bhuang23 <59633478+Bhuang23@users.noreply.github.com>
2021-07-24 16:28:44 -0500

    added props, shopping cart icon, about us component, error component and added isSigning to localStorage

--- src/App.js
+++ src/App.js
@@ -1,59 +1,89 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React, {Component} from "react";
-import Home from './components/home.component';
+import Home from './components/home/home.component';
 import Logincomponent from './components/login/login.component';
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Createusercomponent from "./components/signup/createuser.component";
 import ShopDetailcomponent from "./components/ShopDetail/ShopDetail.component";
 import ShoppingCartcomponent from "./components/ShoppingCart/ShoppingCart.component";
 import Profilecomponent from "./components/profile/profile.component";
+import Errorcomponent from "./components/error/error.component"
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import "./App.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+
 export default class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {isLoggingIn: localStorage.getItem('isloggingIn'), isLoggedIn: localStorage.getItem('loggedIn')};
+        this.state = {isSigningUp: localStorage.getItem('isSigningUp'), isLoggingIn: localStorage.getItem('isloggingIn'), isLoggedIn: localStorage.getItem('loggedIn')};
         this.loginhandler = this.loginhandler.bind(this);
+        this.logouthandler = this.logouthandler.bind(this);
+        this.resethandler = this.resethandler.bind(this);
         console.log(this.state)
     }
 
     componentDidMount() {
-        this.setState({isLoggingIn: localStorage.getItem('isloggingIn'), isLoggedIn: localStorage.getItem('loggedIn')});
+        this.setState({isSigningUp: localStorage.getItem('isSigningUp'), isLoggingIn: localStorage.getItem('isloggingIn'), isLoggedIn: localStorage.getItem('loggedIn')});
+    }
+    resethandler()
+    {
+        localStorage.setItem("loggedIn", "false");
+        localStorage.setItem("isloggingIn", "false");
+        localStorage.setItem("isSigningUp", "false");
+        this.setState({isSigningUp: localStorage.getItem('isSigningUp'), isLoggingIn: localStorage.getItem('isloggingIn'), isLoggedIn: localStorage.getItem('loggedIn')});
     }
     loginhandler()
     {
+        localStorage.setItem("isSigningUp", "false");
         localStorage.setItem("loggedIn", "true");
         localStorage.setItem("isloggingIn", "false");
         this.setState({isLoggingIn: "false", isLoggedIn: "true"});
     }
+    logouthandler()
+    {
+        localStorage.setItem("isSigningUp", "false");
+        localStorage.setItem("username", "");
+        localStorage.setItem("loggedIn", "false");
+        localStorage.setItem("isloggingIn", "true");
+        this.setState({isLoggingIn: "true", isLoggedIn: "false"});
+    }
     login = () => {
+        localStorage.setItem("isSigningUp", "false");
         localStorage.setItem("loggedIn", "false");
         localStorage.setItem("isloggingIn", "true");
         this.setState({isLoggingIn: localStorage.getItem('isloggingIn'), isLoggedIn: localStorage.getItem('loggedIn') });
         console.log(this.state)
 
     }
-    logout = () => {
+    signup = () => {
         localStorage.setItem("loggedIn", "false");
         localStorage.setItem("isloggingIn", "false");
+        localStorage.setItem("isSigningUp", "true");
+        localStorage.setItem("username", "");
+        this.setState({isSigningUp: localStorage.getItem('isSigningUp'), isLoggingIn: localStorage.getItem('isloggingIn'), isLoggedIn: localStorage.getItem('loggedIn') });
+        console.log(this.state)
+    }
+    logout = () => {
+        localStorage.setItem("isSigningUp", "false");
+        localStorage.setItem("loggedIn", "false");
+        localStorage.setItem("isloggingIn", "false");
+        localStorage.setItem("username", "");
         this.setState({isLoggingIn: localStorage.getItem('isloggingIn'), isLoggedIn: localStorage.getItem('loggedIn') });
         console.log(this.state)
     }
 
 render() {
-        const {isLoggingIn, isLoggedIn} = this.state;
-        return (<Router>
-                <div>
+        const {isSigningUp, isLoggingIn, isLoggedIn} = this.state;
+        return (<Router className="background">
+                <div className="background">
                     <Navbar className="app" variant="dark">
-                        <Navbar.Brand href='/'> ShopFirst Ecommerce App </Navbar.Brand>
+                        <Navbar.Brand href='/'>
+                            <ShoppingCartIcon />
+                            &nbsp;ShoppingFirst</Navbar.Brand>
                         <Nav className="ml-auto">
                             <Nav>
-                                {isLoggedIn==="false" && isLoggingIn==="false" && <Link to={'/login-component'} onClick={this.login} className="nav-link">Login</Link>}
-                            </Nav>
-                            <Nav>
-                                {isLoggedIn==="false" && isLoggingIn==="false" && <Link to={'/createuser-component'} className="nav-link">
-                                    Sign up</Link>}
+                                {isLoggedIn==="false" && isLoggingIn==="false" && isSigningUp==="false" && <Link to={'/aboutus-component'} className="nav-link">About us</Link>}
                             </Nav>
                             <Nav>
                                 {isLoggedIn==="true" && isLoggingIn==="false" && <Link to={'/shopdetail-component'} className="nav-link">Shop</Link>}
@@ -69,14 +99,14 @@ render() {
                             </Nav>
                         </Nav>
                     </Navbar>
-                    <hr/>
                     <Switch>
-                        <Route exact path="/" component={Home}/>
-                        <Route path="/login-component" render={props => <Logincomponent loginhandler={this.loginhandler}/>}/>
-                        <Route path="/createuser-component" component={Createusercomponent}/>
+                        <Route exact path="/" render={(props) => <Home resethandler={this.resethandler}/>}/>
+                        <Route path="/login-component" render={(props) => <Logincomponent loginhandler={this.loginhandler} logouthandler={this.logouthandler} login={this.login}/>}/>
+                        <Route path="/createuser-component" render={(props) => <Createusercomponent signup={this.signup}/>}/>
                         <Route path="/shopdetail-component" component={ShopDetailcomponent}/>
                         <Route path="/shoppingcart-component" component={ShoppingCartcomponent}/>
                         <Route path="/profile-component" component={Profilecomponent}/>
+                        <Route path="/error-component" component={Errorcomponent}/>
                     </Switch>
                 </div>
             </Router>
